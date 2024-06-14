44355ee04a03cd4c9e801d009970520d6b1d14b7
Muchiwis <jhordcristian75@gmail.com>
2024-06-14 09:34:56 -0500

    change ruotes

--- routes/web.php
+++ routes/web.php
@@ -24,24 +24,24 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')->secure();
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update')->secure();
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy')->secure();
 
         // VENTAS
-    Route::get('/ventas',[VentasController::class, 'index'])->middleware('can:ventas.home')->name('ventas.index');
+    Route::get('/ventas',[VentasController::class, 'index'])->middleware('can:ventas.home')->name('ventas.index')->secure();
 
-    Route::get('/ventas/create/{id}', [VentasController::class, 'create'])->middleware('can:ventas.home.create')->name('ventas.create');
+    Route::get('/ventas/create/{id}', [VentasController::class, 'create'])->middleware('can:ventas.home.create')->name('ventas.create')->secure();
 
-    Route::get('/ventas/delete/{id}', [VentasController::class, 'destroy'])->middleware('can:ventas.home.create')->name('ventas.delete');
+    Route::get('/ventas/delete/{id}', [VentasController::class, 'destroy'])->middleware('can:ventas.home.create')->name('ventas.delete')->secure();
 
-    Route::get('/ventas/download/{id}', [VentasController::class, 'downloadPDF'])->middleware('can:ventas.home.create')->name('ventas.download');
+    Route::get('/ventas/download/{id}', [VentasController::class, 'downloadPDF'])->middleware('can:ventas.home.create')->name('ventas.download')->secure();
 
-    Route::get('/ventas/actualizar', [VentasController::class, 'actualizarPdf'])->middleware('can:ventas.home.create')->name('ventas.actualizar');
+    Route::get('/ventas/actualizar', [VentasController::class, 'actualizarPdf'])->middleware('can:ventas.home.create')->name('ventas.actualizar')->secure();
 
-    Route::post('ventas/store', [VentasController::class, 'store'])->middleware('can:ventas.home.create')->name('ventas.store');
+    Route::post('ventas/store', [VentasController::class, 'store'])->middleware('can:ventas.home.create')->name('ventas.store')->secure();
 
-    Route::put('ventas/update/{id}', [VentasController::class, 'update'])->middleware('can:ventas.home.create')->name('ventas.update');
+    Route::put('ventas/update/{id}', [VentasController::class, 'update'])->middleware('can:ventas.home.create')->name('ventas.update')->secure();
 });
 
 require __DIR__.'/auth.php';
